c7bf0c76c132281555f6e6d4db02d1ce139a9035
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2021-10-25 12:28:00 -0500

    alter products table positive price and stock only

--- code/ssacg_bknd/ssacgApp/models/products.py
+++ code/ssacg_bknd/ssacgApp/models/products.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.core import validators
 
 
 class Products(models.Model):
     id_product    = models.AutoField(primary_key = True)
     category      = models.CharField(max_length=45)
     name          = models.CharField(max_length=45)
-    stock         = models.IntegerField()
-    unitary_price = models.DecimalField(max_digits = 11, decimal_places = 3)
+    stock         = models.IntegerField(validators=[validators.MinValueValidator(1, message="invalid stock input")])
+    unitary_price = models.DecimalField(max_digits = 11, decimal_places = 3, validators=[validators.MinValueValidator(0.000, message="invalid price input")])
     description   = models.TextField(500)
     
     
